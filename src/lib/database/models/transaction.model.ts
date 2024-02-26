b4070727098b6719ee4588f422b74cf8da12fbe5
unais-k <unaissafa008@gmail.com>
2024-02-26 20:49:05 +0530

    build db connection and schema

---
+++ src/lib/database/models/transaction.model.ts
@@ -0,0 +1,40 @@
+import { Schema, Types, model, models } from "mongoose";
+
+export interface TransactionInterface extends Document {
+  createdAt: Date;
+  stripeId: string;
+  amount: number;
+  plan?: string;
+  credits?: number;
+  buyer: Types.ObjectId | string; 
+}
+
+const TransactionSchema = new Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  stripeId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  plan: {
+    type: String,
+  },
+  credits: {
+    type: Number,
+  },
+  buyer: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+const Transaction = models?.Transaction || model("Transaction", TransactionSchema);
+
+export default Transaction;
